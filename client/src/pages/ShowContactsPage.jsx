41a44284b8481e3c32ab41a707a7c50612188cab
Chengzhe <chengzhewang2@gmail.com>
2025-09-17 15:54:26 +0200

    Retour à la page de login si pas de token

--- client/src/pages/ShowContactsPage.jsx
+++ client/src/pages/ShowContactsPage.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import ShowContacts from "../components/ShowContacts";
 import LogoutButton from "../components/Logout";
+import { Navigate } from "react-router-dom";
 
 export default function RegisterPage() {
-    return (
-    <div>
-        <ShowContacts/>
-        <LogoutButton/>
-    </div>
 
-    );
+    if (localStorage.getItem("token")) {
+        return(
+            <div>
+                <ShowContacts/>
+                <LogoutButton/>
+            </div>
+        )
+        
+    } else {
+        return(
+            <Navigate to="/login" replace />
+        )
+    }
 }
