94eab0118f10fe91b9dd1c0288ce2e7a86d14ee0
Chengzhe <chengzhewang2@gmail.com>
2025-09-17 16:53:02 +0200

    Corrections mineures cors

--- server/src/config/cors.js
+++ server/src/config/cors.js
@@ -3,8 +3,8 @@ const cors = require("cors");
 const corsOptions = {
     origin: [
         "http://localhost:3000",
-        "https://68cac6bbcd557d0008d12e7b--mycontacts-chengzhe.netlify.app/",
-        "https://mycontacts-chengzhe.netlify.app/"
+        "https://68cac6bbcd557d0008d12e7b--mycontacts-chengzhe.netlify.app",
+        "https://mycontacts-chengzhe.netlify.app"
     ],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
