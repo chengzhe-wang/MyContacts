76e636b390caf8836d8284e35d06ceb48df1fb98
Chengzhe <chengzhewang2@gmail.com>
2025-09-17 16:30:31 +0200

    Ajout url déployé cors et api

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,10 @@
 import axios from "axios";
+require('dotenv').config();
+
+const baseURL = process.env.REACT_APP_API_URL || "http://localhost:3001";
 
 const api = axios.create({
-    baseURL: "http://localhost:3001",
+    baseURL: baseURL,
     headers: {
     "Content-Type": "application/json",
     },
--- server/src/config/cors.js
+++ server/src/config/cors.js
@@ -1,7 +1,11 @@
 const cors = require("cors");
 
 const corsOptions = {
-    origin: "http://localhost:3000",
+    origin: [
+        "http://localhost:3000",
+        "https://68cac458e20d10ac5ecf3279--deft-pie-a199db.netlify.app/",
+        "https://deft-pie-a199db.netlify.app/"
+    ],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
